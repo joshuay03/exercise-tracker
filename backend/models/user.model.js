5c97025fe2118e28ffac391d6459fe3345a3b792
Joshua Young <djry1999@gmail.com>
2021-02-17 08:18:11 +1000

    Create routes for users and exercises

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -14,6 +14,6 @@ const userSchema = new Schema ({
     timestamps: true,
 });
 
-const user = mongoose.model('user', userSchema);
+const User = mongoose.model('user', userSchema);
 
 module.exports = User;
