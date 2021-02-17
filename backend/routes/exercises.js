c46bf86977e28c92e8a2de51d9d33db41d35fc32
Joshua Young <djry1999@gmail.com>
2021-02-17 21:12:02 +1000

    Connect frontend to backend of creating components

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -3,11 +3,8 @@ let Exercise = require('../models/exercise.model');
 
 router.route('/').get((req, res) => {
     Exercise.find()
-    .then((exercises) => {
-        res.json(exercises);
-    }).catch((err) => {
-        res.status(400).json('Error: ' + err);
-    });
+    .then(exercises => res.json(exercises))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/add').post((req, res) => {
@@ -15,7 +12,7 @@ router.route('/add').post((req, res) => {
     const description = req.body.description;
     const duration  = Number(req.body.duration);
     const date = Date.parse(req.body.date);
-
+    
     const newExercise = new Exercise({
         username,
         description,
